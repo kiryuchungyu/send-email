5b8ed2b7604b5acb672c37b7a3557ba7c5cdd501
nanoha1470 <nanoha1470@gmail.com>
2022-02-25 02:17:09 +0800

    Push from company and solve the conflict

--- main.py
+++ main.py
@@ -2,21 +2,30 @@
 import smtplib
 from email.message import EmailMessage
 
-msg = EmailMessage()
-msg['Subject'] = input("email subject : ")
-msg['From'] = input("email sender : ")
-msg['To'] = input("email receiver's address : ")
-msg.set_content(input(
-"""email content : 
-"""))
-
-email_list = {'google' : 'smtp.gmail.com',
-              'microsoft' : 'smtp.office365.com',
-}
-server = smtplib.SMTP(email_list['{}'.format(input('what is the email server, google or microsoft? : '))], 587)
-server.ehlo()
-server.starttls()
-server.login(input("your mail : "), input("your password : "))
-server.send_message(msg)
-server.quit()
-print("your mail is sent successfully")
+class Mail:
+    def __init__(self):
+        self.msg = EmailMessage()
+        self.msg['Subject'] = input("email subject : ")
+        self.msg['From'] = input("email sender : ")
+        self.msg['To'] = input("email receiver's address : ")
+        self.msg.set_content(input(
+        """email content : 
+        """))
+  
+    def send_mail(self):
+        email_list = {'google' : 'smtp.gmail.com',
+                      'microsoft' : 'smtp.office365.com',
+        }
+        server = smtplib.SMTP(email_list['{}'.format(input('what is the email server, google or microsoft? : '))], 587)
+        server.ehlo()
+        server.starttls()
+        server.login(input("your mail : "), input("your password : "))
+        server.send_message(self.msg)
+        server.quit()
+        print("your mail is sent successfully")
+        print("git push test")
+        print("second editor test")
+        
+if __name__ == '__main__':
+    main = Mail()
+    main.send_mail()
